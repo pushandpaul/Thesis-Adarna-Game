87e43b53983a0d336f7fe0dd751b2b0138416e7a
Paul Uy <pbu.015@gmail.com>
2016-03-06 15:55:13 +0800

    Aesthetic Additions
    *Candle Light Flicker
    *Moving Clouds
    *Don Juan "Give Item" Animation
    *Don Juan "Side View" State

---
+++ Adarna Unity Project/Assets/Script/CloudSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudSpawn : MonoBehaviour {
+	// Use this for initialization
+	public float yMin;
+	public float yMax;
+
+	public float speed;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
