4cf11321de00c5530882055746164e988e0e4a30
Paul Uy <pbu.015@gmail.com>
2016-05-21 12:32:35 +0800

    Completed Location 'Armenya - Balon'
    Heading
    - Added foregrounds of 'Armenya - Balon' to complete
    
    Aesthetics
    - Added Location '(Minigame) Balon Body'. (Just barebones, still no
    codes for minigame.)
    
    Code
    - Removed parallaxing of y value of a background, because it is
    distracting.

--- Adarna Unity Project/Assets/Script/Parallaxing.cs
+++ Adarna Unity Project/Assets/Script/Parallaxing.cs
@@ -20,7 +20,7 @@ public class Parallaxing : MonoBehaviour {
 	void LateUpdate () {
 		for(int i = 0; i < backgrounds.Length; i++){
 			Vector3 parallax = (previousCameraPosition - transform.position) * (parallaxScales[i] / smoothing);
-			backgrounds[i].position = new Vector3(backgrounds[i].position.x + parallax.x, backgrounds[i].position.y + parallax.y, backgrounds[i].position.z);
+			backgrounds[i].position = new Vector3(backgrounds[i].position.x + parallax.x, backgrounds[i].position.y /*+ parallax.y*/, backgrounds[i].position.z);
 		}
 		previousCameraPosition = transform.position;
 	}
