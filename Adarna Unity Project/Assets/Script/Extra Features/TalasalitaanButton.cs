d91e4bd8b25ade38f6df5c8f0131598ef6300869
Paul Uy <pbu.015@gmail.com>
2016-07-08 14:49:33 +0800

    Assembled Libro ng Kaliwnagan
    Heading^
    - Assembled libro ng kalimwanagan and added functionalities.
    *Showing Halimbawa and Talasalitaan (with cascading)
    *Showing talasalitaan per chapter (replaces existing talasalitaan
    prefabs with new ones every click to a new chapter)
    
    Aesthetics
    - Added a blurred camer which blurs the screen.
    
    Future Tasks
    - Add a method to pause the game
    - Check new say dialogue if it can handle the longest dialogue
    - Uncomment the restriction of talasalitaan for unactivated ones
    - Get number of saknongs in JSON
    - Make back button and libro button functional.

---
+++ Adarna Unity Project/Assets/Script/Extra Features/TalasalitaanButton.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TalasalitaanButton : MonoBehaviour {
+
+	public Text salitaUI;
+	public Text kasingKahuluganUI;
+	public Button button;
+	private Transform halimbawaUI;
+	private TalasalitaanManager talasalitaanManager;
+
+	public string halimbawa;
+	public int clicks = 0;
+
+	void Awake(){
+		salitaUI.text = "????";
+		kasingKahuluganUI.text = "????";
+		talasalitaanManager = FindObjectOfType<TalasalitaanManager>();
+		halimbawaUI = talasalitaanManager.halimbawaUI;
+	}
+
+	public void SetTexts(string salita, string kasingKahulugan, string halimbawa){
+		salitaUI.text = salita;
+		kasingKahuluganUI.text = kasingKahulugan;
+		this.halimbawa = halimbawa;
+	}
+
+	public void ShowExample(){
+
+		foreach(TalasalitaanButton talasalitaanButton in talasalitaanManager.talasalitaanBtns){
+			if(talasalitaanButton != this){
+				talasalitaanButton.clicks = 0;
+			}
+		}
+
+		if(clicks == 0){
+			if(!halimbawaUI.gameObject.activeSelf)
+				halimbawaUI.gameObject.SetActive(true);
+
+			if(this.transform.GetSiblingIndex() < halimbawaUI.GetSiblingIndex()){
+				halimbawaUI.SetSiblingIndex(this.transform.GetSiblingIndex() + 1);
+			}
+			else
+				halimbawaUI.SetSiblingIndex(this.transform.GetSiblingIndex());
+
+			talasalitaanManager.halimbawaUIText.text = halimbawa;
+		}
+
+		else if (clicks == 1){
+			halimbawaUI.gameObject.SetActive(false);
+			clicks = 0;
+		}
+
+		clicks++;
+
+	}
+
+	public void playAudio(){
+		Debug.Log("Play Audio: Work in progress");
+	}
+}
--- Adarna Unity Project/Assets/Script/Extra Features/TalasalitaanManager.cs
+++ Adarna Unity Project/Assets/Script/Extra Features/TalasalitaanManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -8,6 +9,7 @@ public class TalasalitaanManager : MonoBehaviour {
 	[System.Serializable]
 	public class PartData{
 		public int partIndex;
+		public string saknongNumbers;
 		public string summary;
 		public List <Talasalitaan> talasalitaans;
 		bool isFinished;
@@ -22,10 +24,27 @@ public class TalasalitaanManager : MonoBehaviour {
 
 	public string talasalitaanJsonPath;
 
+	//public TalasalitaanButton talasalitaanButtonPrefab;
+	public TalasalitaanButton talasalitaanButtonPrefab;
+	public Transform talasalitaanButtonHolder;
+	public List<TalasalitaanButton> talasalitaanBtns;
+	public Transform summaryUI;
+	public Transform halimbawaUI;
+	public Text halimbawaUIText;
+
+	public GameObject chapterButtonPrefab;
+	public Transform chapterSelectionUI;
+	public Transform chapterSelectedBtn;
+	public List<Transform> chapterSelectionBtns;
+	private GameObject previousChapSelectBtn;
+
 	void Start () {
+		GameObject[] chapSelectBtnHolders = GameObject.FindGameObjectsWithTag("Chapter Button");
 		string jsonString = File.ReadAllText(Application.dataPath + talasalitaanJsonPath);
+		talasalitaanBtns = new List<TalasalitaanButton>();
 
 		partDataList = JsonUtility.FromJson<PartDataList>(jsonString);
+		ShowBook();
 	}
 
 	private void Activate(Talasalitaan talasalitaan){
@@ -73,6 +92,55 @@ public class TalasalitaanManager : MonoBehaviour {
 		return null;
 	}
 
+	public void ShowBook(){
+		ShowPartDataUI(FindObjectOfType<ObjectiveManager>().currentPartIndex);
+	}
+
+	public void ShowPartDataUI(int part){
+		halimbawaUI.gameObject.SetActive(false);
+		chapterSelectedBtn.gameObject.SetActive(true);
+
+		if(previousChapSelectBtn != null)
+			previousChapSelectBtn.SetActive(true);
+
+		if(part == 0){
+			chapterSelectedBtn.GetComponentInChildren<Text>().text = "Tutorial";
+		}
+		else
+			chapterSelectedBtn.GetComponentInChildren<Text>().text = "Kabanata " + part;
+
+		if(chapterSelectionBtns[part].GetSiblingIndex() < chapterSelectedBtn.GetSiblingIndex()){
+			chapterSelectedBtn.SetSiblingIndex(chapterSelectionBtns[part].GetSiblingIndex() + 1);
+		}
+		else
+			chapterSelectedBtn.SetSiblingIndex(chapterSelectionBtns[part].GetSiblingIndex());
+
+		previousChapSelectBtn = chapterSelectionBtns[part].gameObject;
+		chapterSelectionBtns[part].gameObject.SetActive(false);
+
+
+		foreach(TalasalitaanButton talasalitaanBtn in talasalitaanBtns){
+			Destroy(talasalitaanBtn.gameObject);
+		}
+
+		talasalitaanBtns.Clear();
+			
+		foreach(Talasalitaan talasalitaan in partDataList.partsData[part].talasalitaans){
+			TalasalitaanButton talasalitaanButton = (TalasalitaanButton) Instantiate (talasalitaanButtonPrefab);
+
+			talasalitaanButton.transform.SetParent(talasalitaanButtonHolder);
+			talasalitaanButton.transform.localScale = new Vector3(1,1,1);
+			//if(talasalitaan.activated){
+				talasalitaanButton.SetTexts(talasalitaan.salita, talasalitaan.kasingKahulugan, talasalitaan.halimbawa);
+			//}
+			//else
+				//talasalitaanButton.GetComponent<Button>().interactable = false;
+			talasalitaanBtns.Add(talasalitaanButton);
+		}
+
+		summaryUI.GetComponentInChildren<Text>().text = partDataList.partsData[part].summary;
+		//summaryUI.GetComponentInChildren<Image>().sprite =
+	}
 
 	public void RewriteJson(){
 		string jsonString = JsonUtility.ToJson(partDataList);
